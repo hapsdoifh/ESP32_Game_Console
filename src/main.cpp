f176b908e18134164d18f6c64d364c18d8bf3f89
Harry Wang <zuhanwang@outlook.com>
2025-09-15 22:11:10 -0400

    cleaned up main formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdint.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/queue.h>
@@ -33,8 +34,10 @@ typedef struct coords{
 } coords_t;
 
 typedef struct draw_command{
-    coords_t coord;
-    unsigned int color;
+    uint8_t draw_type;
+    coords_t start_coord;
+    coords_t end_coord;
+    int color;
 } draw_command_t;
 
 const int boardSize = 32;
@@ -117,36 +120,25 @@ void do_display(void *pvParameter)
     DrawLine(0,80,127,80,ColorRatio(0.2, 0.3, 0.4));
     DrawEllipse(0,0,127,159,ColorRatio(1, 0, 0));
 
-    // WriteText("DEMO PROGRAM", sizeof("HELLO")/sizeof(char), 40, 40, ColorRatio(1,1,1), ColorRatio(0,0,0));
     WriteText("DEMO", sizeof("DEMO")/sizeof(char), 40, 40, ColorRatio(1,1,1), ColorRatio(0,0,0));
     WriteText("PROGRAM", sizeof("PROGRAM")/sizeof(char), 40, 60, ColorRatio(1,1,1), ColorRatio(0,0,0));
-    int ellipsePosX = 0;
-    int ellipsePosY = 0;
-    int ellipseVeloX = 1;
-    int ellipseVeloY = 1;
-    int ellipseSize = 5;
     vTaskDelay(pdMS_TO_TICKS(1000));
     clearDisplay();
     draw_command_t command;
-
-    int gridSize = 128 / boardSize;
     
     while(1) {
-        // DrawEllipse(ellipsePosX,ellipsePosY,ellipsePosX + ellipseSize,ellipsePosY + ellipseSize,ColorRatio(0, 0, 0));
-        // if(ellipsePosX < 0 || ellipsePosX > max_width - ellipseSize){
-        //     ellipseVeloX *= -1;
-        //     ellipsePosX += ellipseVeloX;
-        // }
-        // if(ellipsePosY < 0 || ellipsePosY > max_height - ellipseSize){
-        //     ellipseVeloY *= -1;
-        //     ellipsePosY+= ellipseVeloY;
-        // }
-        // ellipsePosX += ellipseVeloX;
-        // ellipsePosY+= ellipseVeloY;
-        // vTaskDelay(2);
-        // DrawEllipse(ellipsePosX,ellipsePosY,ellipsePosX + ellipseSize,ellipsePosY + ellipseSize,ColorRatio(r, g, b));
         if(xQueueReceive(commandQueue, (void*)&command, pdMS_TO_TICKS(1000))){
-            DrawRect(command.coord.x * gridSize, command.coord.y * gridSize, command.coord.x * gridSize + gridSize-1, command.coord.y * gridSize + gridSize-1, command.color);
+            switch(command.draw_type){
+                case 0:
+                    DrawRect(command.start_coord.x, command.start_coord.y, command.end_coord.x, command.end_coord.y, command.color);
+                    break;
+                case 1:
+                    DrawEllipse(command.start_coord.x, command.start_coord.y, command.end_coord.x, command.end_coord.y, command.color);
+                    break;
+                default:
+                break;  
+            }
+            
         }
         vTaskDelay(2);
     }
@@ -186,6 +178,29 @@ static void echo_task(void *arg)
     }
 }
 
+void splash_screen(void* pvParameter){
+    int ellipsePosX = 0;
+    int ellipsePosY = 0;
+    int ellipseVeloX = 1;
+    int ellipseVeloY = 1;
+    int ellipseSize = 5;
+    while(true){
+        DrawEllipse(ellipsePosX,ellipsePosY,ellipsePosX + ellipseSize,ellipsePosY + ellipseSize,ColorRatio(0, 0, 0));
+        if(ellipsePosX < 0 || ellipsePosX > max_width - ellipseSize){
+            ellipseVeloX *= -1;
+            ellipsePosX += ellipseVeloX;
+        }
+        if(ellipsePosY < 0 || ellipsePosY > max_height - ellipseSize){
+            ellipseVeloY *= -1;
+            ellipsePosY+= ellipseVeloY;
+        }
+        ellipsePosX += ellipseVeloX;
+        ellipsePosY+= ellipseVeloY;
+        vTaskDelay(2);
+        DrawEllipse(ellipsePosX,ellipsePosY,ellipsePosX + ellipseSize,ellipsePosY + ellipseSize,ColorRatio(r, g, b));
+    }
+}
+
 void snakeGame(void *pvParameter){
     int len = 1;
     coords_t snake[100] = {0};
@@ -201,6 +216,9 @@ void snakeGame(void *pvParameter){
     coords_t cur_food = {0};
     cur_food.x = esp_random() % boardSize;
     cur_food.y = esp_random() % boardSize;
+
+    int gridSize = 128 / boardSize;
+
     while(true){
         cur_head.x += direction[dirSelect][0];
         cur_head.y += direction[dirSelect][1];
@@ -218,65 +236,38 @@ void snakeGame(void *pvParameter){
         draw_command_t command = {0};
         head_ind = (head_ind + 1) % 100;
         snake[head_ind] = cur_head;
-        command.coord = cur_food;
+        command.start_coord.x = cur_food.x * gridSize;
+        command.start_coord.y = cur_food.y * gridSize;
+        command.end_coord.x = command.start_coord.x + gridSize - 1;
+        command.end_coord.y = command.start_coord.y + gridSize - 1;
+
         command.color = ColorRatio(1,0,0);
         xQueueSend(commandQueue, (void*)&command, pdMS_TO_TICKS(1000));
 
-        command.coord = snake[head_ind];
+        command.start_coord.x = snake[head_ind].x * gridSize;
+        command.start_coord.y = snake[head_ind].y * gridSize;
+        command.end_coord.x = command.start_coord.x + gridSize - 1;
+        command.end_coord.y = command.start_coord.y + gridSize - 1;
+
         command.color = ColorRatio(1,1,1);
         xQueueSend(commandQueue, (void*)&command, pdMS_TO_TICKS(1000));
+
+        //if hit food
         if(cur_head.x == cur_food.x && cur_head.y == cur_food.y){
             len++;
             cur_food.x = esp_random() % boardSize;
             cur_food.y = esp_random() % boardSize;
         }else{
-            command.coord = snake[tail_ind];
+            command.start_coord.x = snake[tail_ind].x * gridSize;
+            command.start_coord.y = snake[tail_ind].y * gridSize;
+            command.end_coord.x = command.start_coord.x + gridSize - 1;
+            command.end_coord.y = command.start_coord.y + gridSize - 1;
             command.color = ColorRatio(0,0,0);
             xQueueSend(commandQueue, (void*)&command, pdMS_TO_TICKS(1000));
             tail_ind = (tail_ind + 1) % 100;
         }
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
-}
-
-void recieveUartData(void *pvParameter){
-    // uart_event_t uartEvent;
-    // char data[100];
-    // while(1){
-        
-    //     if(xQueueReceive(commandQueue, (void*)&uartEvent, pdMS_TO_TICKS(1000)) == pdTRUE){
-    //         if(uartEvent.type == UART_DATA){
-    //             int readLen = uart_read_bytes(UART_NUM_0, data, uartEvent.size, pdMS_TO_TICKS(1000));
-    //             data[readLen] = 0;
-    //             ets_printf("input data is: %s", data);
-    //         }
-    //     }
-    // }
-    int num = 0;
-    uint8_t* data = (uint8_t*) malloc(30);
-    while (1) {
-    	int len = sprintf ((char*)data, "Hello world %d\n", num++);
-        uart_write_bytes(UART_NUM_0, data, len);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    free (data);
-}
-
-void uart_setup(){
-    const uart_port_t uart_num = UART_NUM_0;
-    uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .rx_flow_ctrl_thresh = 122,
-    };
-    // Configure UART parameters
-    uart_driver_install(uart_num, 1024, 1024,10,&commandQueue,0);
-    uart_param_config(uart_num, &uart_config);
-    // uart_set_pin(uart_num, 1, 3, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-	// uart_set_mode(uart_num, UART_MODE_RS485_HALF_DUPLEX);
 }
 
 void gpio_setup(){
@@ -302,17 +293,12 @@ extern "C" void app_main()
 {       
 
     gpio_setup();
-    uart_setup();
-    // esp_reset_reason_t reason = esp_reset_reason();
-    // printf("Reset reason: %d\n", reason);
+    esp_reset_reason_t reason = esp_reset_reason();
+    printf("Reset reason: %d\n", reason);
     commandQueue = xQueueCreate(16, sizeof(draw_command_t));
-    // gpio_isr_handler_add(GPIO_NUM_21,onButton,(void*)21);
     xTaskCreate(&do_display, "do disp", 2048,NULL,5,NULL);
     xTaskCreate(&checkButtonDeferred, "check button", 2048,NULL,6,NULL);
-    // xTaskCreate(&recieveUartData, "uart recieve", 4096, NULL, 5, NULL);
     xTaskCreate(echo_task, "USB SERIAL JTAG_echo_task", 4096, NULL, 3, NULL);
     xTaskCreate(snakeGame, "snakeGame", 4096, NULL, 3, NULL);
     
 }
-
-
